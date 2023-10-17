9d82d9cac056e526b4896b147fb18977eec075f0
RyanLCampos <ryanlcampos19@gmail.com>
2023-10-17 12:06:42 -0300

    Atualizando

--- projeto_fertilex/urls.py
+++ projeto_fertilex/urls.py
@@ -16,7 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path, include
 
 urlpatterns = [
@@ -24,3 +25,6 @@ urlpatterns = [
     path('', include('app_fertilex.urls')),
     path('', include('Users.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
